aa959b0c6c5af9c8fdf020ab23642dde74da323b
almeida <hello@eualmeida.com.br>
2026-02-03 18:04:10 -0300

    chore(docs): use docs.Open() func for docs command

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,10 +1,7 @@
 package cmd
 
 import (
-	"os/exec"
-	"runtime"
-
-	"github.com/serenitysz/serenity/internal/exception"
+	"github.com/serenitysz/serenity/internal/cmds/docs"
 	"github.com/spf13/cobra"
 )
 
@@ -12,29 +9,10 @@ var docsCmd = &cobra.Command{
 	Use:   "docs",
 	Short: "Open Serenity documentation in the browser quickly",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return open("https://docs-blond.vercel.app/")
+		return docs.Open()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(docsCmd)
-}
-
-func open(url string) error {
-	var cmd *exec.Cmd
-
-	switch runtime.GOOS {
-	case "darwin":
-		cmd = exec.Command("open", url)
-	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
-	default:
-		cmd = exec.Command("xdg-open", url)
-	}
-
-	if err := cmd.Start(); err != nil {
-		return exception.InternalError("%v", err)
-	}
-
-	return nil
 }
---
+++ internal/cmds/docs/open.go
@@ -0,0 +1,29 @@
+package docs
+
+import (
+	"os/exec"
+	"runtime"
+
+	"github.com/serenitysz/serenity/internal/exception"
+)
+
+const DOCS_URL = "https://docs-blond.vercel.app/"
+
+func Open() error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("open", DOCS_URL)
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", DOCS_URL)
+	default:
+		cmd = exec.Command("xdg-open", DOCS_URL)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return exception.InternalError("%v", err)
+	}
+
+	return nil
+}
